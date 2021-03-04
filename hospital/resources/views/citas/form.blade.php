591e6cff5afe4fde4e1cf22bb69eec1ac44020c8
Chrystian Redin <redin.dev.87@gmail.com>
2021-03-04 07:49:50 -0600

    Update Select Box And Input Date-Time

--- hospital/resources/views/citas/form.blade.php
+++ hospital/resources/views/citas/form.blade.php
@@ -34,17 +34,30 @@
 
 <div class='form-group'>
     <label for="FechaCita">Cita:</label>
-    <input class='form-control' type="datetime" name="FechaCita" id="FechaCita" value='{{isset($cita->FechaCita)?$cita->FechaCita:old("FechaCita")}}'>
+    <input class='form-control' type="datetime-local" name="FechaCita" id="FechaCita" value='{{isset($cita->FechaCita)? date("Y-m-d\TH:i", strtotime($cita->FechaCita)) : date("Y-m-d\TH:i", strtotime(old("FechaCita")))}}'>
 </div>
 
 <div class='form-group'>
     <label for="Id_Doctor">Doctor:</label>
-    <input class='form-control' type="text" name="Id_Doctor" id="Id_Doctor" value='{{isset($cita->Id_Doctor)?$cita->Id_Doctor:old("Id_Doctor")}}'>
+    <select class='form-control' name="Id_Doctor" id="Id_Doctor" value='{{isset($cita->Id_Doctor)?$cita->Id_Doctor:old("Id_Doctor")}}'>
+        <option value="" disabled selected>Selecciona una opcion:</option>
+        <option value="1" {{isset($cita->Id_Doctor)?$cita->Id_Doctor =='1' ? 'selected' : '' : ''}}>Dr. ENRIQUE MARTINEZ</option>
+        <option value="2" {{isset($cita->Id_Doctor)?$cita->Id_Doctor =='2' ? 'selected' : '': ''}}>Dr. ALBERTO MANRIQUEZ</option>
+        <option value="3" {{isset($cita->Id_Doctor)?$cita->Id_Doctor =='3' ? 'selected' : '': ''}}>Dra. LAURA ASCENCIO</option>
+        <option value="4" {{isset($cita->Id_Doctor)?$cita->Id_Doctor =='4' ? 'selected' : '': ''}}>Dr. EDUARDO LOPEZ</option>
+        <option value="5" {{isset($cita->Id_Doctor)?$cita->Id_Doctor =='5' ? 'selected' : '': ''}}>Dra. ANGELICA RODRIGUEZ</option>
+    </select>
 </div>
 
 <div class='form-group'>
     <label for="Id_Departamento">Departamento:</label>
-    <input class='form-control' type="text" name="Id_Departamento" id="Id_Departamento" value='{{isset($cita->Id_Departamento)?$cita->Id_Departamento:old("Id_Departamento")}}'>
+    <select class='form-control' name="Id_Departamento" id="Id_Departamento" value='{{isset($cita->Id_Departamento)?$cita->Id_Departamento:old("Id_Departamento")}}'>
+        <option value="" disabled selected>Selecciona una opcion:</option>
+        <option value="1" {{isset($cita->Id_Departamento)?$cita->Id_Departamento =='1' ? 'selected' : '': ''}}>GINECOLOGÍA Y OBSTETRICIA</option>
+        <option value="2" {{isset($cita->Id_Departamento)?$cita->Id_Departamento =='2' ? 'selected' : '': ''}}>LABORATORIOS CLÍNICOS</option>
+        <option value="3" {{isset($cita->Id_Departamento)?$cita->Id_Departamento =='3' ? 'selected' : '': ''}}>MEDICINA PREVENTIVA</option>
+        <option value="4" {{isset($cita->Id_Departamento)?$cita->Id_Departamento =='4' ? 'selected' : '': ''}}>REHABILITACIÓN</option>
+    </select>
 </div>
 
 <hr>
